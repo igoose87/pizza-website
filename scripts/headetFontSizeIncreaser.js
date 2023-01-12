e05532d359c2b05bd7903b4440d40a39f072eee3
igooseHome <307518@niuitmo.ru>
2023-01-13 02:21:39 +0300

    fix: fixed font-size changing in mobile versions

--- scripts/headetFontSizeIncreaser.js
+++ scripts/headetFontSizeIncreaser.js
@@ -1,3 +1,4 @@
-function resize(elem, percent, padding) {
-    elem.style.fontSize = percent;
+function resize(elem, percent) {
+    if (screen.width > 980)
+        elem.style.fontSize = percent;
 }
